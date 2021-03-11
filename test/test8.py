350f99079c2898a7599c423350b8fb5a6dd69d02
limjongwoo <whddn528@naver.com>
2021-03-11 15:25:46 +0900

    문자열 반복

---
+++ test/test8.py
@@ -0,0 +1,53 @@
+#----알파벳 찾
+# def solution():
+#
+#     alpha_count = [0] * 26
+#
+#     s = input()
+#
+#     if len(s) > 100:
+#         return
+#
+#     s_list = list(s)
+#
+#     # 소문자 알바펫 돌면서 포함되어있는 지 확인
+#     # 있는 경우 알파벳의 인덱스 찾기
+#     # alpha_count에 인덱스 값 넣기
+#
+#     for i in range(97, 123):
+#         if chr(i) in s:
+#             index = s_list.index(chr(i))
+#             alpha_count[i - 97] = index
+#         else:
+#             alpha_count[i - 97] = -1
+#
+#     for value in alpha_count:
+#         print(value)
+
+
+#----문자열 반복
+def solution():
+
+    case = int(input())
+    result = ""
+
+    for _ in range(case):
+        r, s = input().split()
+
+        if len(s) <1 or len(s) > 20:
+            return
+
+        num = int(r)
+
+        for value in s:
+            for _ in range(num):
+                result += value
+        result += " "
+    result_arr = result.split()
+
+    for value in result_arr:
+        print(value)
+solution()
+
+
+
