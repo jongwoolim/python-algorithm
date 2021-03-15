0cc3f3bdfcd7d03324c7ab5cc9b9524932f3fa82
limjongwoo <whddn528@naver.com>
2021-03-15 13:40:08 +0900

    다익스트라-힙구현

--- dijkstra.py
+++ dijkstra.py
@@ -1,4 +1,5 @@
 import sys
+import heapq
 
 #inputs = sys.stdin.readline()
 INF = int(1e9) # 무한을 의미하는 값 10억 설정
@@ -32,25 +33,46 @@ def get_smallest_node():
             index = i
     return index
 
+# def dijkstra(start):
+#     # 시작 노드에 대해서 초기화
+#     distance[start] = 0
+#     visited[start] = True
+#     for j in graph[start]:
+#         distance[j[0]] = j[1]
+#
+#     # 시작 노드를 제외한 전체 n -1 개의 노드에 대해 반복
+#     for i in range(n-1):
+#         # 현재 최단 거리가 가장 짧은 노드를 꺼내서 방문 처리
+#
+#         now = get_smallest_node()
+#         visited[now] = True
+#         # 현재 노드와 연결된 다른 노드를 확인
+#         for j in graph[now]:
+#             cost = distance[now] + j[1]
+#             # 현재 노드를 거쳐서 다른 노드로 이동하는 거리가 더 짧은 경우
+#             if cost < distance[j[0]]:
+#                 distance[j[0]] = cost
+
+#----다익스트라 성능 개선 구현 방법
+#----visitied 테이블 사용x heap라이브러리 사용o
 def dijkstra(start):
-    # 시작 노드에 대해서 초기화
+    q= []
+
+    # 시작 노드로 가기 위한 최단 거리는 0으로 설정하여, 큐에 삽입
+    heapq.heappush(q, (0, start))
     distance[start] = 0
-    visited[start] = True
-    for j in graph[start]:
-        distance[j[0]] = j[1]
 
-    # 시작 노드를 제외한 전체 n -1 개의 노드에 대해 반복
-    for i in range(n-1):
-        # 현재 최단 거리가 가장 짧은 노드를 꺼내서 방문 처리
+    while q: # 큐가 비어있지 않다면
+        # 가장 최단 거리가 짧은 노드에 대한 정보 꺼내기
+        dist, now = heapq.heappop(q)
 
-        now = get_smallest_node()
-        visited[now] = True
-        # 현재 노드와 연결된 다른 노드를 확인
-        for j in graph[now]:
-            cost = distance[now] + j[1]
-            # 현재 노드를 거쳐서 다른 노드로 이동하는 거리가 더 짧은 경우
-            if cost < distance[j[0]]:
-                distance[j[0]] = cost
+        # 현재 노드와 연결된 다르 인접한 노드들을 확인
+        for i in graph[now]:
+            cost = dist + i[1]
+            # 현재 노드를 거쳐서, 다른 노드로 이동하는 거리가 더 짧은 경우
+            if cost < distance[i[0]]:
+                distance[i[0]] = cost
+                heapq.heappush(q, (cost, i[0]))
 
 dijkstra(start)
 
--- heap.py
+++ heap.py
@@ -10,22 +10,16 @@ def heapsort(iterable):
     #모든 원소를 차례대로 힙에 삽입 ,, 삽입 제거시 -부호를 붙이면 max-Heap
     for value in iterable:
         heapq.heappush(h, value)
+        # heapq.heappush(h, -value) 최대 힙 입력
 
     #힙에 삽입된 모든 원소를 차례대로 꺼내어 담기
     for i in range(len(h)):
         result.append(heapq.heappop(h))
+        # result.append(-heapq.heappop(h)) 최대 힙 꺼내기
     
     return result
 
-n = int(input())
-arr = []
-
-for i in range(n):
-    arr.append(int(input())
-
-res = heapsort(arr)
-
-for i in range(n):
-    print(res[i])
+result = heapsort([1, 3, 5, 7, 9, 2, 4, 6, 8, 0])
+print(result)
 
 
