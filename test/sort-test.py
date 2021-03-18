90b9215e2ab8f4ffede16742d92c744cff147c3d
limjongwoo <whddn528@naver.com>
2021-03-18 21:01:35 +0900

    수 정렬하기2

--- test/sort-test.py
+++ test/sort-test.py
@@ -1,15 +1,31 @@
 import sys
-def solution():
+import heapq
 
-    n = int(sys.stdin.readline())
-    arr = []
+# input = sys.stdin.readline
+#----수 정렬하기2
+def heapsort(iterable):
+    h = []
+    result = []
 
-    for _ in range(n):
-        num = int(input())
-        arr.append(num)
+    # 모든 원소를 차례대로 힙에 삽입 ,, 삽입 제거시 -부호를 붙이면 max-Heap
+    for value in iterable:
+        heapq.heappush(h, value)
+        # heapq.heappush(h, -value) 최대 힙 입력
 
-    arr.sort()
+    # 힙에 삽입된 모든 원소를 차례대로 꺼내어 담기
+    for i in range(len(h)):
+        result.append(heapq.heappop(h))
+        # result.append(-heapq.heappop(h)) 최대 힙 꺼내기
 
-    for v in arr:
-        print(v)
-solution()
+    return result
+
+n = int(input())
+inputs = []
+for _ in range(n):
+    m = int(input())
+    inputs.append(m)
+result = heapsort(inputs)
+
+for v in result:
+    print(v)
+
