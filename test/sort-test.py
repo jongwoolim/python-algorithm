f9c2bd8514b0191b401eef0cc0b64d5fe8363720
limjongwoo <whddn528@naver.com>
2021-03-19 12:42:13 +0900

    수 정렬하기3

--- test/sort-test.py
+++ test/sort-test.py
@@ -3,29 +3,38 @@ import heapq
 
 # input = sys.stdin.readline
 #----수 정렬하기2
-def heapsort(iterable):
-    h = []
-    result = []
+# def heapsort(iterable):
+#     h = []
+#     result = []
+#
+#     # 모든 원소를 차례대로 힙에 삽입 ,, 삽입 제거시 -부호를 붙이면 max-Heap
+#     for value in iterable:
+#         heapq.heappush(h, value)
+#         # heapq.heappush(h, -value) 최대 힙 입력
+#
+#     # 힙에 삽입된 모든 원소를 차례대로 꺼내어 담기
+#     for i in range(len(h)):
+#         result.append(heapq.heappop(h))
+#         # result.append(-heapq.heappop(h)) 최대 힙 꺼내기
+#
+#     return result
+#
+# n = int(input())
+# inputs = []
+# for _ in range(n):
+#     m = int(input())
+#     inputs.append(m)
+# result = heapsort(inputs)
+#
+# for v in result:
+#     print(v)
 
-    # 모든 원소를 차례대로 힙에 삽입 ,, 삽입 제거시 -부호를 붙이면 max-Heap
-    for value in iterable:
-        heapq.heappush(h, value)
-        # heapq.heappush(h, -value) 최대 힙 입력
+#---- 수 정렬하기3
+import sys
+input = sys.stdin.readline
+m=10001;l=[0]*m
+for _ in range(int(input())):l[int(input())]+=1
+for i in range(1,m):print(f"{i}\n"*l[i],end="")
 
-    # 힙에 삽입된 모든 원소를 차례대로 꺼내어 담기
-    for i in range(len(h)):
-        result.append(heapq.heappop(h))
-        # result.append(-heapq.heappop(h)) 최대 힙 꺼내기
 
-    return result
-
-n = int(input())
-inputs = []
-for _ in range(n):
-    m = int(input())
-    inputs.append(m)
-result = heapsort(inputs)
-
-for v in result:
-    print(v)
 
